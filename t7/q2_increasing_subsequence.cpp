b26a53eb33e95c03bcf5a7b4099163a570e335c5
itay-mor <69759941+itay-mor@users.noreply.github.com>
2023-04-29 17:36:32 +0000

    Improve t7q2

--- t7/q2_increasing_subsequence.cpp
+++ t7/q2_increasing_subsequence.cpp
@@ -4,56 +4,43 @@
 
 using namespace std;
 
-int64_t find_first_bigger(vector<int64_t>& vec, int64_t number) {
-  int64_t begin = -1;
-  int64_t end = vec.size();
-
-  while ((end-begin)!=1) {
-    if (vec[(begin+end)/2] < number) {
-      begin = (begin+end)/2;
-    }
-    else {
-      end = (begin+end)/2;
-    }
-    
-  }
-  
-  return end;
-}
-
-// int64_t find_longest_increasing_subsequence(vector<int64_t>& sequence) {
-//   vector<int64_t> vec;
-//   for (int64_t number : sequence) {
-//     if (vec.size() == 0) {
-//       vec.push_back(number);
-//     } else if (vec[vec.size()-1] < number) {
-//       vec.push_back(number);
-//     } else {
-//       vec[find_first_bigger(vec, number)] = number;
-//     }
-//   }
-
-//   return vec.size();
-// }
-int64_t find_longest_increasing_subsequence() {
+int64_t find_longest_increasing_subsequence(const vector<int64_t>& sequence) {
   vector<int64_t> vec;
-  int64_t number;
-  int64_t n; cin >> n;
-  for (int64_t i = 0; i < n; i++) {
-    cin >> number;
-    if (vec.size() == 0) {
-      vec.push_back(number);
-    } else if (vec[vec.size()-1] < number) {
+  for (int64_t number : sequence) {
+    // Binary search for first element that is equal or greater than number.
+    auto it = lower_bound(vec.begin(), vec.end(), number);
+    if (it == vec.end()) {
+      // Larger than the tail (or first number); start a new column with it.
       vec.push_back(number);
     } else {
-      vec[find_first_bigger(vec, number)] = number;
+      // Replace the element with our number.
+      *it = number;
     }
   }
-
   return vec.size();
 }
+
+vector<int64_t> read_sequence() {
+  int64_t size;
+  cin >> size;
+
+  int64_t number;
+  vector<int64_t> sequence;
+  sequence.reserve(size);
+  for (int64_t i = 0; i < size; i++) {
+    cin >> number;
+    sequence.push_back(number);
+  }
+
+  return sequence;
+}
+
+int64_t run() {
+  vector<int64_t> sequence = read_sequence();
+  return find_longest_increasing_subsequence(sequence);
+}
+
 int main() {
-  vector<int64_t> sequence = {13, 7, 10};
-  cout << find_longest_increasing_subsequence() << endl;
+  cout << run() << endl;
   return 0;
 }
