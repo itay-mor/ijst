b82d860ebf14ff94ae704411ecaff6423b4726b9
Omer Mor <omer.mor@gmail.com>
2023-09-16 13:35:49 +0300

    Update GridPaths.cpp

--- year2/t1/CSES Introductory Problems/GridPaths.cpp
+++ year2/t1/CSES Introductory Problems/GridPaths.cpp
@@ -10,14 +10,14 @@ using namespace std;
 
 int64_t number_of_paths(string& path) {
   // Stop condition
-  vector<tuple<int, int>> used_squares = {{0,0}};
+  vector<tuple<int, int>> used_squares = {{0, 0}};
   tuple<int, int> start = {0, 0};
 
-  int64_t solution = 0;
-  int first_unknown = -1;
+  int64_t solutions = 0;
+  int first_unknown_index = -1;
   for (int i = 0; i < path.length(); ++i) {
     if (path[i] == '?') {
-      first_unknown = i;
+      first_unknown_index = i;
       break;
     }
     auto& [x, y] = start;
@@ -32,36 +32,34 @@ int64_t number_of_paths(string& path) {
         break;
     }
 
-
-    if (x >= GRID_SIZE || x < 0 || y >= GRID_SIZE || y < 0) {
+    if (x < 0 || GRID_SIZE <= x ||
+        y < 0 || GRID_SIZE <= y) {
       return 0;
     }
-    for (auto used_square : used_squares) {
+    for (const auto& used_square : used_squares) {
       if (used_square == start) return 0;
     }
 
     used_squares.push_back(start);
   }
 
-  if (first_unknown == -1) return (start == make_tuple(0, GRID_SIZE - 1)) ? 1 : 0;
+  if (first_unknown_index == -1) return (start == make_tuple(0, GRID_SIZE - 1)) ? 1 : 0;
 
+  path[first_unknown_index] = 'R';
+  solutions += number_of_paths(path);
 
-  path[first_unknown] = 'R';
-  solution += number_of_paths(path);
+  path[first_unknown_index] = 'L';
+  solutions += number_of_paths(path);
 
+  path[first_unknown_index] = 'D';
+  solutions += number_of_paths(path);
 
-  path[first_unknown] = 'L';
-  solution += number_of_paths(path);
+  path[first_unknown_index] = 'U';
+  solutions += number_of_paths(path);
 
-  path[first_unknown] = 'D';
-  solution += number_of_paths(path);
+  path[first_unknown_index] = '?';
 
-  path[first_unknown] = 'U';
-  solution += number_of_paths(path);
-
-  path[first_unknown] = '?';
-
-  return solution;
+  return solutions;
 }
 
 int main() {
