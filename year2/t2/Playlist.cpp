63ba3f9ae1473d06c637dd8c8ff61fdef261f34f
Omer Mor <omer.mor@gmail.com>
2023-09-25 17:47:38 +0300

    update

--- year2/t2/Playlist.cpp
+++ year2/t2/Playlist.cpp
@@ -20,25 +20,24 @@ void run_playlist() {
   int64_t n;
   cin >> n;
 
-  auto songs = read_vector(n);
+  const auto songs = read_vector(n);
 
-  map<int64_t, vector<int64_t>::iterator> m = {{*songs.begin(), songs.begin()}};
+  map<int64_t, vector<int64_t>::const_iterator> id_to_iterator = {{*songs.begin(), songs.begin()}};
 
   auto left_it = songs.begin();
   auto right_it = songs.begin();
 
   int64_t max_length = 0, curr_length = 1;
-  while (right_it < songs.end()-1) {
-    right_it++;
-    auto it = m.find(*right_it);
-    if (it != m.end()) {
+  while (++right_it < songs.end()) {
+    auto it = id_to_iterator.find(*right_it);
+    if (it != id_to_iterator.end()) {
       left_it = it->second + 1;
-      m.clear();
+      id_to_iterator.clear();
       for (auto i = left_it; i <= right_it; i ++) {
-        m.emplace(*i, i);
+        id_to_iterator.emplace(*i, i);
       }
     }
-    m.emplace(*right_it, right_it);
+    id_to_iterator.emplace(*right_it, right_it);
 
     curr_length = right_it-left_it + 1;
     if (curr_length > max_length) {
