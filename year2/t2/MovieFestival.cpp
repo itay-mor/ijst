520e83267ed160631c4ea898174f8fc0d5dfa155
Itay Mor <itay.mor.09@gmail.com>
2023-09-26 12:03:28 +0300

    Solved Movie Festival

---
+++ year2/t2/MovieFestival.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Itay Mor on 26/09/2023.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using Movie = pair<int64_t, int64_t>;
+
+//struct Movie {
+//  int64_t _start_hour;
+//  int64_t _end_hour;
+//
+//  Movie() {
+//
+//  }
+//  Movie(int64_t start_hour, int64_t end_hour) {
+//    _start_hour = start_hour;
+//    _end_hour = end_hour;
+//  }
+//
+//  bool operator<(const Movie& other) const {
+//    return _end_hour < other._start_hour;
+//  }
+//
+//  bool operator>(const Movie& other) const {
+//    return _start_hour > other._end_hour;
+//  }
+//};
+
+vector<Movie> read_movies(size_t size) {
+  Movie input_movie;
+  vector<Movie> movies(size);
+  for (size_t i = 0; i < size; ++i) {
+    cin >> input_movie.first >> input_movie.second;
+    movies[i] = input_movie;
+  }
+
+  return movies;
+}
+
+void run_movie_festival() {
+  size_t n;
+  cin >> n;
+  auto movies = read_movies(n);
+  vector<int64_t> dp(n + 1, 0);
+  sort(movies.begin(), movies.end());
+
+  for (int64_t i = n - 1; i >= 0; i--) {
+    auto [start_hour, end_hour] = movies[i];
+    auto next_available_movie_index = lower_bound(movies.begin(),
+                                                  movies.end(),
+                                                  Movie(end_hour, 0))
+        - movies.begin();
+    dp[i] = max(1 + dp[next_available_movie_index], dp[i + 1]);
+  }
+
+  cout << dp[0];
+}
+
+int main() {
+  run_movie_festival();
+}
